cdc6951ec99199cd25da15566bc27b2361895e1c
Kondra96 <o.krechnak@seznam.cz>
2025-05-01 19:29:01 +0200

    working remove button, fetching from API

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -3,52 +3,44 @@
         <h1>To-Do List made with Vue</h1>
         <AddNewTask />
         <ul>
-            <li>
+            <li v-for="task in tasks" :key="task.id">
                 <article>
-                    <h2>Lorem Lorem Lorem</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptates ratione exercitationem
-                        numquam itaque, porro nisi deserunt eveniet aliquid consequuntur rerum non et voluptas, debitis
-                        iure
-                        inventore! Quo, nihil. Quisquam.</p>
-                </article>
-            </li>
-            <li>
-                <article>
-                    <h2>Lorem Lorem Lorem</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure laborum quo eveniet fugit expedita
-                        suscipit voluptates architecto! Ut inventore eveniet culpa nostrum, corrupti delectus
-                        perspiciatis
-                        modi hic quia distinctio cupiditate omnis, ullam, magni sit porro minus numquam aut! Nemo sunt
-                        et
-                        adipisci quibusdam numquam explicabo ea, doloremque, soluta ducimus quis, dolorum eaque odit
-                        assumenda rem quod mollitia earum amet obcaecati debitis facilis tempora officiis eius? Quasi
-                        eum
-                        ullam magni, voluptas veniam sed. Repellat architecto reiciendis aperiam quaerat molestias iusto
-                        odio omnis rem amet excepturi ducimus nesciunt, voluptate quas id aspernatur. Vero vel
-                        exercitationem distinctio iusto eveniet necessitatibus harum velit aspernatur maxime, eos
-                        consequatur voluptates, explicabo accusamus nostrum veritatis doloremque, illo odit voluptas
-                        blanditiis dolor odio ab quas corporis. Porro sunt, modi enim reprehenderit ducimus possimus
-                        provident rem dolorum architecto fuga nemo nesciunt dolores accusamus et similique deserunt
-                        voluptas
-                        eaque? Libero nesciunt consequatur cupiditate maxime suscipit sint praesentium voluptate
-                        quibusdam
-                        aliquam eaque amet ea, tempore voluptatibus quisquam nisi? Deserunt consectetur eos animi
-                        necessitatibus rerum incidunt iure voluptate repellendus consequuntur, illum ratione natus,
-                        molestiae tempora tenetur voluptatum dolorum eaque quidem doloribus? Magnam eius, labore
-                        adipisci
-                        laudantium eligendi esse quae vero quisquam quod ab saepe facilis. Sapiente vel temporibus dicta
-                        architecto iusto quae tempore inventore qui numquam facere! Non, sint magni optio incidunt
-                        consequatur esse officiis asperiores iusto hic tempore. Blanditiis aspernatur inventore ratione
-                        voluptas ipsa debitis vel? Quasi quo libero accusantium iure laboriosam quas officia explicabo
-                        dolor, qui facilis a, temporibus, modi delectus expedita! Molestiae sequi temporibus nisi cum
-                        dolorem. Praesentium, architecto!</p>
+                    <header>
+                        <h2>{{ task.name }}</h2>
+                        <button @click="removeTask(task.id)">Remove</button>
+                    </header>
+                    <p>{{ task.description }}</p>
                 </article>
             </li>
         </ul>
     </section>
 </template>
 <script setup>
+import { ref, onMounted } from 'vue';
 import AddNewTask from './AddNewTask.vue';
+
+const tasks = ref([]);
+
+async function getTasks() {
+    try {
+        const response = await fetch('https://hp-api.onrender.com/api/spells');
+        const data = await response.json();
+        tasks.value = data;
+    } catch (error) {
+        console.error('Error fetching tasks: ', error);
+        return [];
+    }
+}
+
+function removeTask(id) {
+    const index = tasks.value.findIndex((task) => task.id === id);
+    if (index !== -1) {
+        tasks.value.splice(index, 1);
+    }
+}
+
+onMounted(getTasks);
+
 </script>
 <style scoped lang="scss">
 #to-do-list {
@@ -76,6 +68,8 @@ import AddNewTask from './AddNewTask.vue';
         padding: 0;
 
         li {
+            min-height: 10rem;
+
             padding: .75rem;
 
             border-radius: .5rem;
@@ -83,11 +77,29 @@ import AddNewTask from './AddNewTask.vue';
             background-color: rgba($color: #ffffff, $alpha: 0.15);
 
             article {
-                h2 {
-                    margin-bottom: .5rem;
+                display: flex;
+                flex-direction: column;
+
+                height: 100%;
+
+                header {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+
+                    h2 {
+                        margin-bottom: .5rem;
+                    }
+
+                    button {
+                        padding-block: .25rem;
+                        padding-inline: .75rem;
+                    }
                 }
+
                 p {
                     margin: 0;
+                    margin-bottom: .5rem;
                 }
             }
 
@@ -98,6 +110,10 @@ import AddNewTask from './AddNewTask.vue';
 @media screen and (max-width: 600px) {
     #to-do-list {
         padding-inline: 5vw;
+
+        ul {
+            grid-template-columns: 1fr;
+        }
     }
 }
 </style>
